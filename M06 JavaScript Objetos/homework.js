0370b956cd55e86b0d9c05437fc34af3ef26938a
ivis <ivisruiz177@gmail.com>
2023-07-22 17:12:51 -0300

    tareas 06

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,13 +7,29 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+   var obj = {
+      nombre: nombre,
+      edad: edad,
+      meow: function meow(){
+         return "Meow!";
+      }
 }
+
+return obj;
+}
+ 
 
 function nuevoUsuario(nombre, email, password) {
    // Debes crear un nuevo objeto.
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
-   // Tu código:
+   // Tu código: 
+   var obj = {
+      nombre: "nombre",
+      email: "email",
+      password: "password"
+   } 
+   return obj;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +38,8 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   objeto[propiedad]= null;
+   return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,20 +47,26 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   objeto[metodo]();
+   
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+   return objetoMisterioso.numeroMisterioso * 5
+
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
-}
-
+   var eliminar = {};
+      eliminar.objeto =['propiedad'];
+   delete eliminar.objeto;
+}return eliminar
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
